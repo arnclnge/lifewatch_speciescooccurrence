e71259f2472f8491bbc44ad98a4bf2213bb6e230
arnclnge <arnclnge821@gmail.com>
2022-11-17 08:23:13 +0000

    slight updates

--- data_vis.R
+++ data_vis.R
@@ -5,12 +5,12 @@ library(glatos) #R package for the Great Lakes Acoustic Telemetry
 library(tibble)
 library(readr)
 
-setwd("~/Ari/CoOccur/")
-plot_loc <- "~/Ari/CoOccur/plots/"
+setwd("~/lifewatch_speciescooccurrence/")
+plot_loc <- "~/lifewatch_speciescooccurrence/plots/"
 
-detections_merge <- read_csv("DPH_cod_sb.csv")
-df_dol <- read_csv("DPH_dolphins.csv")
-df_porpoise2 <- read_csv("DPH_porpoise.csv")
+detections_merge <- read_csv("csv/DPH_cod_sb.csv")
+df_dol <- read_csv("csv/DPH_dolphins.csv")
+df_porpoise2 <- read_csv("csv/DPH_porpoise.csv")
 
 #rename time column
 colnames(df_dol)[2] <- "detection_timestamp_utc"
@@ -26,7 +26,7 @@ abacusPlot(df_dol[df_dol$dol_DPH >0,], detColNames = list(locationCol = "locatio
            x_res = "year",x_format = "%b-%Y", Ylab="")
 
 #DOT PLOT
-df = read_csv("DPH_final.csv")
+df = read_csv("csv/DPH_final.csv")
 
 df_plot = df %>% 
   select(
@@ -41,13 +41,14 @@ df_plot = df %>%
 
 #convert df from wide to long
 df_long <- gather(as.data.frame(df_plot), species, dph,por_DPH:cod_DPH)
+df_long$day_time <- as.Date(df_long$day_time)
 
 #rename
 df_long$Season <- recode(df_long$Season, "December Solstice" = "Winter", "September Equinox" = "Autumn", "March Equinox"="Spring", "June Solstice"="Summer")
 df_long$location_col <- recode(df_long$location_col,"bpns-Belwindreefballs-CPOD"="bpns-Belwindreefballs","bpns-Cpowerreefballs-CPOD"="bpns-Cpowerreefballs")
 
 df_long %>% filter(dph==1) %>% ggplot(aes(x = day_time, y = location_col, color = Season))+
-  geom_point()+theme_bw()+theme(axis.title=element_blank(),strip.text.x = element_text(size = 15))+
+  geom_point()+theme_bw()+theme(axis.title=element_blank(),strip.text.x = element_text(size = 15),axis.text.x=element_text(size =7, angle=20))+
   scale_color_manual(values = c("Winter" = "steelblue",
                                 "Autumn"="deeppink",
                                 "Summer"="darkorange",
@@ -55,7 +56,9 @@ df_long %>% filter(dph==1) %>% ggplot(aes(x = day_time, y = location_col, color 
   facet_wrap(~species,labeller = labeller(species = c("por_DPH" = "Harbour Porpoise (PAM)",
                                                       "sb_DPH" = "European Seabass (AT)",
                                                       "cod_DPH" = "Atlantic Cod (AT)",
-                                                      "dol_DPH" = "Dolphins (PAM)")))
+                                                      "dol_DPH" = "Dolphins (PAM)")))+
+  scale_x_date(date_labels = "%b-%Y",date_breaks = "3 months")
+
 ggsave(paste0(plot_loc,"dph_year.png"), device='png', dpi=500, width=13, height=7)
 
 #MAP STATIONS
